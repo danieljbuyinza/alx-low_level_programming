8b71f5114c2a504981ae0ecb554ea2796982d5f2
danieljbuyinza <danieljbuyinza@gmail.com>
2023-02-26 15:20:21 -0800

    Write a C program that prints using the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
+/* Outputing a string */
+
 /**
- * Outputting a string
- *
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char myString[] = "\"Programming is like building a multilingual puzzle";
