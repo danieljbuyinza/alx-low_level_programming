5179fa0e426ce744d6ebe53b2cf27920510d3342
danieljbuyinza <danieljbuyinza@gmail.com>
2023-02-26 14:51:18 -0800

    Write a C program that prints using the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-	char myString[] = "Programming is like building a multilingual puzzle";
+	char myString[] = "\"Programming is like building a multilingual puzzle";
 	puts(myString);
 	return 0;
 }
