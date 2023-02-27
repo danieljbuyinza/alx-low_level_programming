6b89ffb83f091febf4953096dcc73895f941bb38
danieljbuyinza <danieljbuyinza@gmail.com>
2023-02-27 01:57:09 -0800

    Write a C program that prints without using printf and puts functions

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,13 +9,13 @@
  *
  * nbyte - Variable holding integer
  */
+int write(int filedes, const char *buf, unsigned int nbyte);
 
 /**
  * main - Entry point
  *
  * Return: 1 (Error)
  */
-int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
