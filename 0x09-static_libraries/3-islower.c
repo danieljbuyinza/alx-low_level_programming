4a137d385b5eb924bf0e5c4d183c52ba208963a0
danieljbuyinza <danieljbuyinza@gmail.com>
2023-03-17 12:14:01 -0700

    Create the static library libmy.a

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * @c: Given character
+ * Return: 0
+*/
+int _islower(int c)
+{
+        return (0);
+}
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _putchar - Entry point
+ * @c: Given character
+ * Return: 0
+*/
+int _putchar(char c)
+{
+        return (0);
+}
