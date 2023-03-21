5de3e226e4b15c2eb5e00c404b58b0abc1295ef4
danieljbuyinza <danieljbuyinza@gmail.com>
2023-03-21 16:09:01 -0700

    Add new line to file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,8 +1,4 @@
-struct dog {
-	char *name;
-	float age;
-	char *owner;
-};
+#include "dog.h"
 
 /**
  * init_dog - Initializes a variable of type struct dog
@@ -11,9 +7,11 @@ struct dog {
  * @age: Dog's age
  * @owner: Dog's owner
 */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
 }
+
