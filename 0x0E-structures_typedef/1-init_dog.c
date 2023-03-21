7a94340e0af2f4f49dd30650e9a118badffa52e8
danieljbuyinza <danieljbuyinza@gmail.com>
2023-03-21 14:44:06 -0700

    Remove includes and strcpy

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,3 @@
-#include <stdlib.h>
-#include "dog.h"
-
 /**
  * init_dog - Initializes a variable of type struct dog
  * @d: Dog struct
@@ -11,7 +8,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	strcpy(d->name, name);
+	d->name = name;
 	d->age = age;
-	strcpy(d->owner, owner);
+	d->owner = owner;
 }
