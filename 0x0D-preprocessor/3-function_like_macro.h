60e0f604ce38131ccf438e6191e227094f089b7f
danieljbuyinza <danieljbuyinza@gmail.com>
2023-03-20 23:36:38 -0700

    Convert ABS macro to function

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,15 @@
 #ifndef 3_FUNCTION_LIKE_MACRO_H
 #define 3_FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ((x) < 0 ? (x) * -1 : (x))
+/**
+ * ABS - Return absolute value
+ * @x: Given number
+ *
+ * Return: int
+*/
+int ABS(x)
+{
+	return ((x) < 0 ? (x) * -1 : (x));
+}
 
 #endif
