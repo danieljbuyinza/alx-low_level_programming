b53cc88d6803dc6fb82b0ef6b574292f0462eb6b
danieljbuyinza <danieljbuyinza@gmail.com>
2023-03-21 14:32:29 -0700

    Remove if check

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -12,11 +10,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-	{
-		d = malloc(sizeof(struct dog));
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
