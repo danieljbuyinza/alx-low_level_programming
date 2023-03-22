ab638a4f064ca5ec13ea820a9130dfdb8d3213a0
danieljbuyinza <danieljbuyinza@gmail.com>
2023-03-21 17:31:27 -0700

    Convert from . to ->

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,18 +10,18 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if (d.name != NULL)
-			printf("Name: %s\n", d.name);
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)\n");
 
-		if (d.age != NULL)
-			printf("Age: %s\n", d.age);
+		if (d->age != NULL)
+			printf("Age: %s\n", d->age);
 		else
 			printf("(nil)\n");
 
-		if (d.owner != NULL)
-			printf("Owner: %s\n", d.owner);
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
 		else
 			printf("(nil)\n");
 	}
