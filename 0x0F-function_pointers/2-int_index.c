56b07c0483315d7cdad36282b9d2636daad8287f
danieljbuyinza <danieljbuyinza@gmail.com>
2023-03-22 17:28:28 -0700

    Reorder statements

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,21 +11,19 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	if (size <= 0)
+		return (-1);
+	
 	int n = 0;
 
 	if (array != NULL && cmp != NULL)
 	{
-		if (size <= 0)
-			return (-1);
-		else
+		for (n = 0; n < size; n++)
 		{
-			for (n = 0; n < size; n++)
-			{
-				if (cmp(array[n] != 0)
-					return (n);
-				return (-1);
-			}
+			if (cmp(array[n] != 0)
+				return (n);
 		}
+		return (-1);
 	}
 }
 
