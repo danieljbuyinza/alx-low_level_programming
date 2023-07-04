eaf2f4d30cf0c4d8780ab4aa52aad951d3b6a34d
danieljbuyinza <danieljbuyinza@gmail.com>
2023-07-04 06:44:54 -0700

    Add function return and if check in case program is in directory

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,10 +10,16 @@
 int main(int argc, char *argv[])
 {
 	const char *program;
+	const char *lastSlash = strrchr(program, '/');
 
 	*program = argv[0];
 
+	if (lastSlash != NULL)
+		program = lastSlash + 1;
+
 	_putchar(program);
 	_putchar('\n');
+
+	return (0);
 }
 
