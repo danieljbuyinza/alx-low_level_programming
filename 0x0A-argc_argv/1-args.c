79efe0283e458f57258872379c3d3732c70ec1eb
danieljbuyinza <danieljbuyinza@gmail.com>
2023-07-04 07:45:28 -0700

    Subtract the count of the program itself.

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,9 +7,9 @@
  * @argv: Argument vector
  * Return: 0
 */
-int main(int argc, char __attribute__((unused))  *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
-	printf("%s\n", argc);
+	printf("%s\n", argc - 1);
 
 	return (0);
 }
