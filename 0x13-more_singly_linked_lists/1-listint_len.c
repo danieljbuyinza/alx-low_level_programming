8454692afbd05413f0fde1e82e8e698239521108
danieljbuyinza <danieljbuyinza@gmail.com>
2023-07-04 13:57:33 -0700

    Remove unnecessary code/variables.

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -9,15 +8,13 @@
 size_t listint_len(const listint_t *h)
 {
 	size_t no_of_elements;
-	const listint_t *curent;
 
 	no_of_elements = 0;
-	*current = h;
 
-	while (current != NULL)
+	while (h)
 	{
 		no_of_elements++;
-		current = current->next;
+		h = h ->next;
 	}
 
 	return (no_of_elements);
