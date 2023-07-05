4233d228bdb173d94ab301f9338e19fd0dd4ac9a
danieljbuyinza <danieljbuyinza@gmail.com>
2023-07-05 02:25:16 -0700

    Write a function that prints a message before the main function is executed.

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * tortoise_challenges_hare - Prints message before main function
+*/
+void tortoise_challenges_hare(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+
