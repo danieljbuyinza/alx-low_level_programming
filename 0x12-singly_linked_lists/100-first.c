cd79a4bc4c290d16b6c6d758d41745383d036b77
danieljbuyinza <danieljbuyinza@gmail.com>
2023-07-05 16:25:00 -0700

    Refactor code.

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
+void first(void) __attribute__ ((constructor));
+
 /**
  * tortoise_challenges_hare - Prints message before main function
 */
 void tortoise_challenges_hare(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	print("I bore my house upon my back!\n");
 }
 
