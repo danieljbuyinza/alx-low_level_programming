225f19d4e146667c82e4272e83a4024077a9dc39
danieljbuyinza <danieljbuyinza@gmail.com>
2023-07-05 15:55:02 -0700

    Change logic.

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,18 +10,23 @@
 */
 int main(int argc, char *argv[])
 {
-	int cents, coins, num_of_coins, count, i;
+	int cents;
+	int coins[];
+	int num_coins;
+	int count, i;
 
-	cents = atoi(argv[1]);
+	coins[] = {25, 10, 5, 2, 1};
+	num_coins = sizeof(coins) / sizeof(coins[0]);
 	count = 0;
-	coins[5] = {25, 10, 5, 2, 1};
-	num_of_coins = sizeof(coins) / sizeof(coins[0]);
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	cents = atoi(argv[1]);
+
 	if (cents < 0)
 	{
 		printf("0\n");
@@ -30,11 +35,14 @@ int main(int argc, char *argv[])
 
 	for (i = 0; i < num_coins; i++)
 	{
-		count += cents / coins[i];
-		cents %= coins[i];
+		while (cents >= coins[i])
+		{
+			cents -= coins[i];
+			count++;
+		}
 	}
-	printf("%d\n", count);
 
+	printf("%d\n", count);
 	return (0);
 }
 
