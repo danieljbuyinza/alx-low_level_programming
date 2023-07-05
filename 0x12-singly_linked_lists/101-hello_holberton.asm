e047d8d71955dd6057219acee364b919d4d4afdf
danieljbuyinza <danieljbuyinza@gmail.com>
2023-07-05 16:19:13 -0700

    Change implementation.

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,24 +1,10 @@
-section .data
-    hello db 'Hello, Holberton', 0
-    format db '%s', 0
-
-section .text
-    global _start
-
-_start:
-    ; Prepare arguments for printf
-    mov rdi, format
-    mov rsi, hello
-    xor rax, rax   ; Clear rax for printf function
-
-    ; Call printf function
-    call printf
-
-    ; Exit the program
-    mov eax, 60   ; System call number for exit
-    xor edi, edi  ; Exit status 0
-    syscall
-
-section .data
-    extern printf
+	global   main
+	  extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
 
