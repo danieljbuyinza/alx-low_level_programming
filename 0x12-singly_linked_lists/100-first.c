581b06b2e2339c2b45419f05c3d58c8606e28347
danieljbuyinza <danieljbuyinza@gmail.com>
2023-07-05 16:31:32 -0700

    Correct typo in printf.

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
+void tortoise_challenges_hare(void) __attribute__ ((constructor));
+
 /**
  * tortoise_challenges_hare - Prints message before main function
 */
 void tortoise_challenges_hare(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-	print("I bore my house upon my back!\n");
+	printf("I bore my house upon my back!\n");
 }
 
