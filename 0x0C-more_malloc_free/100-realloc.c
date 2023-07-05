727b6fc4e3f921317579e13a8611d5a68bc3b5ea
danieljbuyinza <danieljbuyinza@gmail.com>
2023-07-05 08:29:22 -0700

    Reorder pointer initialisation.

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,9 +13,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 	char *char_ptr, *char_new_ptr;
 
-	char_ptr = (char *)ptr;
-	char_new_ptr = (char *)new_ptr;
-
 	if (new_size == 0)
 	{
 		free(ptr);
@@ -34,6 +31,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 		return (NULL);
+
+	char_ptr = (char *)ptr;
+	char_new_ptr = (char *)new_ptr;
 
 	for (i = 0; i < old_size; i++)
 		char_new_ptr[i] = char_ptr[i];
