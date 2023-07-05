ad5ffc25c867c711cda17933de019fde31b2cdc6
danieljbuyinza <danieljbuyinza@gmail.com>
2023-07-04 18:58:24 -0700

    Move initialisations to be with declarations.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,13 +11,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node, *temp;
 
+	temp = *head;
+	new_node = malloc(sizeof(listint_t));
+
 	if (head == NULL)
 		return (NULL);
 
 	if (idx == 0)
 		return (add_nodeint(head, n));
-
-	temp = *head;
 
 	while (idx > 1 && temp != NULL)
 	{
@@ -28,7 +29,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (temp == NULL)
 		return (NULL);
 
-	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 
