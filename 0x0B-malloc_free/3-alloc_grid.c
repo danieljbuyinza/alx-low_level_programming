449b932c7804217e8e5898a32643d989e0cfffa9
danieljbuyinza <danieljbuyinza@gmail.com>
2023-08-08 16:50:56 -0700

    Correct declaration within for loop.

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 
 		if (elements[i] == NULL)
 		{
-			for (int j = 0; j < i; j++)
+			for (j = 0; j < i; j++)
 				free(elements[j]);
 			free(elements);
 			return (NULL);
