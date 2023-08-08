d9352d45aabb13e4a09ad92c888bce4e337c8743
danieljbuyinza <danieljbuyinza@gmail.com>
2023-08-08 16:42:37 -0700

    Add free if memory allocation fails.

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,6 +22,12 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		elements[i] = malloc(sizeof(int) * width);
+
+		if (elements[i] == NULL)
+		{
+			free(elements);
+			return (NULL);
+		}
 	}
 
 	for (i = 0; i < height; i++)
