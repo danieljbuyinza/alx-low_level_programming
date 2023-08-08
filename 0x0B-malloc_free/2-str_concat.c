ef47099980644842ad7a1b43ebe764d9e0552695
danieljbuyinza <danieljbuyinza@gmail.com>
2023-08-08 13:26:47 -0700

    Concatenate the strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,6 +11,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *concatenated;
+	size_t i;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -20,6 +21,15 @@ char *str_concat(char *s1, char *s2)
 	concatenated = malloc(sizeof(char) * ((strlen(s1) + strlen(s2)) + 1));
 	if (concatenated == NULL)
 		return (NULL);
+
+	for (i = 0; i < strlen(s1); i++)
+	{
+		concatenated[i] = s1[i];
+	}
+	for (i = 0; i < strlen(concatenated); i++)
+	{
+		concatenated[strlen(s1) + i] = s2[i];
+	}
 
 	return (concatenated);
 }
