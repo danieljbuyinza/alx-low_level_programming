8385fd6248db6f8d131e7ae83ba58b9c00d2135e
danieljbuyinza <danieljbuyinza@gmail.com>
2023-08-14 17:45:02 -0700

    Define a new type struct dog with the elements name, age and owner.

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Describing a dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ *
+ * Description: Information about a dog, including its name,
+ * age, and owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
