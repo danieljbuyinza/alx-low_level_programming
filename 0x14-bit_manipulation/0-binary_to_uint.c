3614e235c109b8260e438f774bb4be1154e1bd51
danieljbuyinza <danieljbuyinza@gmail.com>
2023-08-31 19:19:02 -0700

    Declare loop iterator with other variables at the beginning of the function.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,7 +10,7 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n, decimal, base, remainder;
+	unsigned int n, i, decimal, base, remainder;
 
 	decimal = 0;
 	base = 1;
@@ -18,7 +18,7 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	for (int i = 0; b[i] != '\0'; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
