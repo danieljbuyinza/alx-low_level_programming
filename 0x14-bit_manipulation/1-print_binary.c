a887f6771867aad1b2114f2db8e96016489942b4
danieljbuyinza <danieljbuyinza@gmail.com>
2023-08-31 20:00:17 -0700

    Convert to character.

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,12 +9,12 @@
 void print_binary(unsigned long int n)
 {
 	if (n == 0 || n == 1)
-		_putchar(n);
+		_putchar('0' + n);
 
 	if (n > 1)
 		print_binary(n / 2);
 
-	_putchar(num % 2);
+	_putchar('0' + num % 2);
 	_putchar('\n');
 }
 
