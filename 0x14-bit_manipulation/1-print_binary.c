d0f5ae7ce83685c7f8c515d8349592c8b11f2e2e
danieljbuyinza <danieljbuyinza@gmail.com>
2023-08-31 19:57:51 -0700

    Correct typos.

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,10 +11,10 @@ void print_binary(unsigned long int n)
 	if (n == 0 || n == 1)
 		_putchar(n);
 
-	if (num > 1)
-		print_binary(num / 2);
+	if (n > 1)
+		print_binary(n / 2);
 
 	_putchar(num % 2);
-	_putchar("\n");
+	_putchar('\n');
 }
 
