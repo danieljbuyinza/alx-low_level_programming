eeae577bfece53e22c37a5b3ae3e9bb13f1c0059
danieljbuyinza <danieljbuyinza@gmail.com>
2023-10-02 20:19:38 -0700

    Change logic.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include "main.h"
 
 /**
  * read_textfile - Reads text file and prints it to POSIX standard output
@@ -11,42 +9,32 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file;
-	ssize_t bytes_read, bytes_written;
-	char *buffer;
+	FILE *file;
+	ssize_t text_read, text_written;
+	char buffer[letters];
 
 	if (filename == NULL)
 		return (0);
 
-	file = open(filename, O_RDONLY);
-	if (file == -1)
+	file = fopen(filename, "r");
+	if (file == NULL)
 		return (0);
 
-	buffer = malloc(letters);
-	if (buffer == NULL)
+	text_read = fread(buffer, sizeof(char), letters, file);
+	if (text_read < 0)
 	{
-		close(file);
+		fclose(file);
 		return (0);
 	}
 
-	bytes_read = read(file, buffer, letters);
-	if (bytes_read == -1)
+	text_written = write(STDOUT_FILENO, buffer, text_read);
+	if (text_written != text_read)
 	{
-		free(buffer);
-		close(file);
+		fclose(file);
 		return (0);
 	}
 
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		free(buffer);
-		close(file);
-		return (0);
-	}
-
-	free(buffer);
-	close(file);
-	return (bytes_written);
+	fclose(file);
+	return (text_read);
 }
 
