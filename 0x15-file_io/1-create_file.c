ee775f30ddfe95b8628deb7a3f1a2d23a41badd8
danieljbuyinza <danieljbuyinza@gmail.com>
2023-10-05 12:10:18 -0700

    Change of logic.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,8 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, write_result;
+	int fd;
+	ssize_t text_length, write_result;
 
 	if (filename == NULL)
 		return (-1);
@@ -23,8 +24,17 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		write_result = write(fd, text_content, strlen(text_content));
+		text_length = 0;
+		while (text_content[text_length] != '\0')
+			text_length++;
+
+		write_result = write(fd, text_content, text_length);
 		if (write_result == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+		if (write_result != text_length)
 		{
 			close(fd);
 			return (-1);
