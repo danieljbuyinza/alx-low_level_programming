de7ce90173b60af5fb320e6bdc84cf994b1c937c
danieljbuyinza <danieljbuyinza@gmail.com>
2023-10-05 06:28:09 -0700

    Alter code to use required functions.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,41 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include "main.h"
 
 /**
  * read_textfile - Reads text file and prints it to POSIX standard output
- * @filename: Name of given file
+ * @filename: Name of the given file
  * @letters: Number of letters it should read and print
  * Return: Actual number of letters it could read and print
-*/
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *file;
+	int file_descriptor;
 	ssize_t text_read, text_written;
-	char buffer[letters];
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	file = fopen(filename, "r");
-	if (file == NULL)
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
 		return (0);
 
-	text_read = fread(buffer, sizeof(char), 1, file);
-	while (text_read > 0 && letters > 0)
+	buffer = malloc(letters);
+	if (buffer == NULL)
 	{
-		text_written = write(STDOUT_FILENO, buffer, text_read);
-		if (text_written != text_read)
-		{
-			fclose(file);
-			return (0);
-		}
-
-		letters--;
-		text_read = fread(buffer, sizeof(char), 1, file);
+		close(file_descriptor);
+		return (0);
 	}
 
-	fclose(file);
+	text_read = read(file_descriptor, buffer, letters);
+	if (text_read == -1)
+	{
+		close(file_descriptor);
+		free(buffer);
+		return (0);
+	}
+
+	text_written = write(STDOUT_FILENO, buffer, text_read);
+	if (text_written != text_read)
+	{
+		close(file_descriptor);
+		free(buffer);
+		return (0);
+	}
+
+	close(file_descriptor);
+	free(buffer);
 	return (text_read);
 }
 
