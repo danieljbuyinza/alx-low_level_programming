e2aa148ceff524fdb7cc9e2c3502e9ec84753d42
danieljbuyinza <danieljbuyinza@gmail.com>
2023-10-05 11:47:15 -0700

    Use symbolic constants.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,26 +12,26 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int file_descriptor, write_result;
+	int fd, write_result;
 
 	if (filename == NULL)
 		return (-1);
 
-	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (file_descriptor == -1)
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, UNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		write_result = write(file_descriptor, text_content, strlen(text_content));
+		write_result = write(fd, text_content, strlen(text_content));
 		if (write_result == -1)
 		{
-			close(file_descriptor);
+			close(fd);
 			return (-1);
 		}
 	}
 
-	close(file_descriptor);
+	close(fd);
 	return (1);
 }
 
